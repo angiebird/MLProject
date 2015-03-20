950b4044c871d2ca8df8c956f322b640889fe789
Angie Chiang <angieyayabird@gmail.com>
2015-03-19 20:39:32 -0400

    functionalize

--- src/yelp.py
+++ src/yelp.py
@@ -24,9 +24,22 @@ class Idxer:
     def __init__(self):
         self.docId = 0
         self.rgx = re.compile("[\.\?\n\!]")
+        self.rgx2 = re.compile("[\,\ \-]")
         self.invertedIdx = {}
         self.docLs = []
         self.sentenceNum = 0
+        self.stopWord = {}
+        self.loadStopWord()
+    def loadStopWord(self):
+        fp = open("./stopword")
+        line = fp.readline()
+        while line:
+            line = stem(line.strip())
+            self.stopWord[line] = True
+            line = fp.readline()
+        #for word in self.stopWord.keys():
+        #    print word
+            
     def addDoc(self, doc):
         ri = self.docId
         self.docId += 1
@@ -34,7 +47,7 @@ class Idxer:
         self.docLs.append([])
         self.sentenceNum += len(sentenceLs)
         for si, sentence in enumerate(sentenceLs):
-            wordLs = sentence.strip().lower().split()
+            wordLs = self.rgx2.split(sentence.strip().lower())
             wordLs = [stem(word) for word in wordLs]
             for word in wordLs:
                 if word not in self.invertedIdx:
@@ -47,6 +60,50 @@ class Idxer:
 def intersection(ls0, ls1):
     return filter(lambda x: x in ls0, ls1)
 
+def mutualInfo(px, py, pxy):
+    px_ = px - pxy
+    p__ = 1 - px - py + pxy
+    p_y = py - pxy
+    v0 = pxy * np.log(pxy/(px*py+1e-5))
+    v1 = px_ * np.log(px_/(px*(1-py)+1e-5))
+    v2 = p_y * np.log(p_y/((1-px)*py)+1e-5)
+    v3 = p__ * np.log(p__/((1-px)*(1-py)+1e-5))
+    return v0 + v1 + v2 + v3
+
+def retrieveSentence(idxer, word):
+    print "Result of search:", word
+    relWordDic = {}
+    if word in idxer.invertedIdx:
+        docLs = idxer.invertedIdx[word]
+        for doc in docLs:
+            ri = doc/1000
+            si = doc%1000
+            print " ".join(idxer.docLs[ri][si])
+
+def retrieveMutualInfo(idxer, word):
+    print "Result of MI related to:", word
+    relWordDic = {}
+    if word in idxer.invertedIdx:
+        docLs = idxer.invertedIdx[word]
+        for doc in docLs:
+            ri = doc/1000
+            si = doc%1000
+            #print " ".join(idxer.docLs[ri][si])
+            wordLs = idxer.docLs[ri][si]
+            for term in wordLs:
+                if term not in relWordDic:
+                    sentenceNum = idxer.sentenceNum
+                    docLs2 = idxer.invertedIdx[term]
+                    interDocLs= intersection(docLs , docLs2)
+                    pxy = float(len(interDocLs))/sentenceNum
+                    px  = float(len(docLs))/sentenceNum
+                    py  = float(len(docLs2))/sentenceNum
+                    relWordDic[term] = mutualInfo(px, py, pxy)                        
+    relWordLs = sorted(relWordDic.items(), key=lambda x: x[1], reverse = True)
+    for i in xrange(min(20, len(relWordLs))):
+        print relWordLs[i]
+
+
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -56,36 +113,7 @@ if __name__ == "__main__":
     while True:
         word = raw_input('Search?')
         word = stem(word.strip().lower())
-        print word
-        relWordDic = {}
-        if word in idxer.invertedIdx:
-            docLs = idxer.invertedIdx[word]
-            for doc in docLs:
-                ri = doc/1000
-                si = doc%1000
-                print " ".join(idxer.docLs[ri][si])
-                wordLs = idxer.docLs[ri][si]
-                for term in wordLs:
-                    if term not in relWordDic:
-                        sentenceNum = idxer.sentenceNum
-                        docLs2 = idxer.invertedIdx[term]
-                        interDocLs= intersection(docLs , docLs2)
-                        pxy = float(len(interDocLs))/sentenceNum
-                        px  = float(len(docLs))/sentenceNum
-                        py  = float(len(docLs2))/sentenceNum
-                        px_ = px - pxy
-                        p__ = 1 - px - py + pxy
-                        p_y = py - pxy
-                        v0 = pxy * np.log(pxy/(px*py+1e-5))
-                        v1 = px_ * np.log(px_/(px*(1-py)+1e-5))
-                        v2 = p_y * np.log(p_y/((1-px)*py)+1e-5)
-                        v3 = p__ * np.log(p__/((1-px)*(1-py)+1e-5))
-                        relWordDic[term] = v0 + v1 + v2 + v3
-                        
-
-            relWordLs = sorted(relWordDic.items(), key=lambda x: x[1], reverse = True)
-            for i in xrange(min(100, len(relWordLs))):
-                print relWordLs[i]
+        retrieveMutualInfo(idxer, word)
 
 
 
